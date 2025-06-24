1e04db3b9303aa254dc1a6465261604a5227e413
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-24 14:47:32 -0500

    Update supabase.js

--- vnx-notebook-backend/supabase.js
+++ vnx-notebook-backend/supabase.js
@@ -2,7 +2,12 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_KEY
 );
+
+export const TABLES = {
+  NOTES: 'notes',
+  USERS: 'users'
+};
 
 export { supabase };
